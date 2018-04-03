70a80a7c38a6aa17d4fa80a8b8a1ce7308684a30
Jack Wohlfert <jack.wohlfert@gmail.com>
2018-04-02 22:00:02 -0400

    Always run scripts

--- Dockerfile
+++ Dockerfile
@@ -52,7 +52,7 @@ RUN echo @testing http://nl.alpinelinux.org/alpine/edge/testing >> /etc/apk/repo
       --with-png-dir=/usr/include/ \
       --with-jpeg-dir=/usr/include/ && \
     #curl iconv session
-    docker-php-ext-install pdo_mysql mysqli gd exif intl xsl json soap dom zip opcache && \
+    docker-php-ext-install pdo_mysql pcntl mysqli gd exif intl xsl json soap dom zip opcache  && \
     pecl install xdebug && \
     docker-php-source delete && \
     mkdir -p /etc/nginx && \
--- scripts/start.sh
+++ scripts/start.sh
@@ -184,15 +184,13 @@ else
 fi
 
 # Run custom scripts
-if [[ "$RUN_SCRIPTS" == "1" ]] ; then
-  if [ -d "/var/www/html/scripts/" ]; then
-    # make scripts executable incase they aren't
-    chmod -Rf 750 /var/www/html/scripts/*; sync;
-    # run scripts in number order
-    for i in `ls /var/www/html/scripts/`; do /var/www/html/scripts/$i ; done
-  else
+if [ -d "/var/www/html/scripts/" ]; then
+  # make scripts executable incase they aren't
+  chmod -Rf 750 /var/www/html/scripts/*; sync;
+  # run scripts in number order
+  for i in `ls /var/www/html/scripts/`; do /var/www/html/scripts/$i ; done
+else
     echo "Can't find script directory"
-  fi
 fi
 
 if [ -z "$SKIP_COMPOSER" ]; then
